0a9365e440a064e95a99a08d80d22413604795cc
Eliam <Texsword76@gmail.com>
2023-11-10 17:05:18 +0100

    Initial Commit

---
+++ GourmetGO/GourmetGOApp.swift
@@ -0,0 +1,17 @@
+//
+//  GourmetGOApp.swift
+//  GourmetGO
+//
+//  Created by Eliam on 10/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct GourmetGOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GourmetGOUITests/GourmetGOUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GourmetGOUITestsLaunchTests.swift
+//  GourmetGOUITests
+//
+//  Created by Eliam on 10/11/2023.
+//
+
+import XCTest
+
+final class GourmetGOUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
